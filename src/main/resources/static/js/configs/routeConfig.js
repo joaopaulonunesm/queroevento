87a154c50c13f31f9d3f43a8299ac96495efe910
Joao Marques <joaopaulonunesm@hotmail.com>
2017-08-29 14:41:54 -0300

    Implementando telas do sistema

--- src/main/resources/static/js/configs/routeConfig.js
+++ src/main/resources/static/js/configs/routeConfig.js
@@ -3,6 +3,18 @@ angular.module("queroEventoApp").config(function ($routeProvider) {
 	$routeProvider.when("/", {
 		templateUrl: "views/index/home.html"
 	});
+
+	$routeProvider.when("/login", {
+		templateUrl: "views/index/login.html"
+	});
+
+	$routeProvider.when("/category/:name", {
+		templateUrl: "views/index/categoria.html"
+	});
+
+	$routeProvider.when("/event/:name", {
+		templateUrl: "views/index/evento.html"
+	});
 		
 	$routeProvider.otherwise({redirectTo: "/"});
 	
