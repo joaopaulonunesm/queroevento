b0ce7be7637db41b7bfe32a127926234c4ac5719
Joao Marques <joaopaulonunesm@hotmail.com>
2017-08-30 14:52:19 -0300

    Iniciando implementação do painel administrativo

--- src/main/resources/static/js/configs/routeConfigAdmin.js
+++ src/main/resources/static/js/configs/routeConfigAdmin.js
@@ -3,6 +3,22 @@ angular.module("queroEventoApp").config(function ($routeProvider) {
 	$routeProvider.when("/", {
 		templateUrl: "views/admin/home.html"
 	});
+
+	$routeProvider.when("/event/insert", {
+		templateUrl: "views/admin/component/novoEvento.html"
+	});
+
+	$routeProvider.when("/event/list", {
+		templateUrl: "views/admin/component/eventos.html"
+	});
+
+	$routeProvider.when("/offer", {
+		templateUrl: "views/admin/component/ofertas.html"
+	});
+
+	$routeProvider.when("/settings", {
+		templateUrl: "views/admin/component/configuracao.html"
+	});
 		
 	$routeProvider.otherwise({redirectTo: "/"});
 	
